9357cdcb6ffc4f37c12c22618211020b87411d2a
Goutham <arun.goutham@capgemini.com>
2019-08-08 16:53:16 +0530

    1

--- main.tf
+++ main.tf
@@ -1,27 +1,65 @@
-provider "aws" {
-  region     = "${var.vpc_region}"
+terraform {
+  required_version = ">= 0.12.0"
 }
 
-provider "http" {}
+provider "aws" {
+  version = ">= 2.11"
+  region  = "${var.region}"
+}
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  cluster_name = "test-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "~> v1.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.6.0"
 
-  name = "barclays-vpc"
-  cidr = "10.0.0.0/16"
+  name                 = "test-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
-  azs             = ["${slice(data.aws_availability_zones.available.names, 0, 3)}"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
 
-  tags = "${
-    map(
-     "Name", "terraform-eks-demo-node",
-     "kubernetes.io/cluster/${var.cluster-name}", "shared",
-    )
-  }"
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,3 @@
-variable "vpc_region" {
-    default = "us-east-1"  
-}
-
-data "aws_region" "current" {}
-
-data "aws_availability_zones" "available" {}
-
-variable "cluster-name" {
-  default = "terraform-eks-demo"
-  type    = "string"
+variable "region" {
+  default = "us-east-2"
 }
