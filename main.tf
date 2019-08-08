d6b95549567fd016e2fe478d6c4d866d79317190
Goutham <arun.goutham@capgemini.com>
2019-08-08 17:56:53 +0530

    4

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ module "bastion-server" {
 
   vpc-security-group-ids = "${module.sg-bastion.ec2-sg-security-group}"
   #ec2-subnets-ids = "${module.vpc.public-subnet-ids}"
-  ec2-subnets-ids = "${element(module.vpc.public_subnet, 1)}"
+  ec2-subnets-ids = "${element(module.vpc.public_subnets, 1)}"
   #vpc-id = "${module.vpc.vpc-id}"
 
   #IN CASE OF LAUNCHING EC2 IN SPECIFIC SUBNETS OR PRIVATE SUBNETS, PLEASE UN-COMMENT BELOW"
