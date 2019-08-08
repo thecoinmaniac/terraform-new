1952543de7dbb6992caaecd74418ec0d1a9f13a7
Goutham <arun.goutham@capgemini.com>
2019-08-08 17:30:37 +0530

    1

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "region" {
   description = "AWS region."
   value       = var.region
 }
-
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = aws_subnet.private.*.id
-}
