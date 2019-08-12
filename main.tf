78c9f5a47cb1bdd1b03f31743d12fb68a886ed36
Goutham <arun.goutham@capgemini.com>
2019-08-12 10:19:52 +0530

    1

--- main.tf
+++ main.tf
@@ -124,9 +124,8 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/8",
-      "0.0.0.0/0,"
-    ]
+      "10.0.0.0/16",
+      ]
   }
 }
 
